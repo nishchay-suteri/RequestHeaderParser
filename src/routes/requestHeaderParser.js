9763c5b69bfc0e5bb145b3fb036e48b87165514f
nishchay271095 <nishchay271095@gmail.com>
2020-10-06 22:25:00 +0530

    Send the Response after parsing the Header

--- src/routes/requestHeaderParser.js
+++ src/routes/requestHeaderParser.js
@@ -3,7 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.send(' Request received');
+    const output_format = {
+        ipaddress: req.ip,
+        language: req.get('accept-language'),
+        software: req.get('user-agent')
+    };
+    res.json(output_format);
 })
 
 module.exports = router;
